0c776de9ae76ff63fa3fba902340786aea508307
Ayman <oumgharayman2004@gmail.com>
2024-12-02 19:43:24 +0100

    first commit

---
+++ expense-manager-react_2/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { ExpenseEntryList } from "./ExpenseEntryList";
+function App() {
+return (
+<div className="App">
+      <ExpenseEntryList />
+    </div>
+);
+}
+export default App;
